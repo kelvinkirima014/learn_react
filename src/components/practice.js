fd98a4f70f7f514a6d901d7ae1e828c76e63a642
kelvinkirima014 <kelvinkirima2@gmail.com>
2021-01-20 14:57:52 +0300

    wrote a function that returns time of day

---
+++ src/components/practice.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function MyInfo() {
+    return(
+        <div>
+            <input type="checkbox"/>
+            <p>placeholder</p>
+            
+        </div>
+      
+        
+
+        )
+}
+export default MyInfo;
